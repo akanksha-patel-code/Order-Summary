facb18663a749a9ee537ca7f12a16b8db30938b0
Akanksha Patel <a.patel@easternenterprise.com>
2021-09-22 17:19:59 +0530

    updated css

--- order-summary/src/components/OrderSummary/OrderSummaryRate/index.js
+++ order-summary/src/components/OrderSummary/OrderSummaryRate/index.js
@@ -4,7 +4,7 @@ export const OrderSummaryRate = (props) => {
   return (
     <div id="OrderSummaryChangeLink">
       {props.children}
-      <div>Annual Plan</div>
+      <div id="Plan">Annual Plan</div>
       <div>$59.99/year</div>
     </div>
   );
